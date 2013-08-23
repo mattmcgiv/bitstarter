d61cc2c57204952f78bfe1a0c71426e33b897dcb
mcgivney <matthew.mcgivney@gmail.com>
2013-08-23 18:51:53 -0400

    Updated node.js server to listen on localhost rather than static IP.

--- web.js
+++ web.js
@@ -1,43 +1,17 @@
-var http = require('http');
+#!/usr/bin/env node
+   
+var express = require('express');
 var fs = require('fs');
-var path = require('path');
-http.createServer(function (request, response) {
-    console.log('request starting...');
-	
-	var filePath = '.' + request.url;
-	if (filePath == './')
-		filePath = './index.html';
-		
-	var extname = path.extname(filePath);
-	var contentType = 'text/html';
-	switch (extname) {
-		case '.js':
-			contentType = 'text/javascript';
-			break;
-		case '.css':
-			contentType = 'text/css';
-			break;
-	}
-	
-	path.exists(filePath, function(exists) {
-	
-		if (exists) {
-			fs.readFile(filePath, function(error, content) {
-				if (error) {
-					response.writeHead(500);
-					response.end();
-				}
-				else {
-					response.writeHead(200, { 'Content-Type': contentType });
-					response.end(content, 'utf-8');
-				}
-			});
-		}
-		else {
-			response.writeHead(404);
-			response.end();
-		}
-	});
-	
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+var htmlfile = "index.html";
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);	
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
